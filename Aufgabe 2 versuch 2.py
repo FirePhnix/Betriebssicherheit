695c68b5afea4353a61e961a20a83a40dc6f4a8e
FirePhnix <fabio-merkt@t-online.de>
2023-05-16 16:41:03 +0200

    print eingefügt

--- Aufgabe 2 versuch 2.py
+++ Aufgabe 2 versuch 2.py
@@ -1,5 +1,6 @@
 import graphviz
-import matplotlib.pyplot as plt
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Users/fabio/anaconda3/Library/bin/graphviz'
 
 class BLOCK:
 	def __init__(self,name, reliability):
@@ -16,8 +17,15 @@ class BLOCK:
 		#...
 		return reliability
 	
-    def draw(self, graph):
-        graph.node(self.name, label=self.name)
+	def draw(self, graph):
+		graph.node(self.name, label=self.name)
+
+	def print(self):
+		
+
+		for block in self.blocks:
+			block.draw(graph)
+
 
 	
 class SEQBLOCK:
@@ -37,15 +45,22 @@ class SEQBLOCK:
 	def rel(self):
 		reliability = 1.0
 		for block in self.blocks:
-        	reliability *= block.rel()
+			reliability *= block.rel()
 		return reliability
 
 	def draw(self, graph):
-        with graph.subgraph(name='cluster_' + self.name) as c:
-            c.attr(style='dashed')
-            c.node(self.name, shape='box', label=self.name)
-            for block in self.blocks:
-                block.draw(c)
+		with graph.subgraph(name='cluster_' + self.name) as c:
+			c.attr(style='dashed')
+			c.node(self.name, shape='box', label=self.name)
+			for block in self.blocks:
+				block.draw(c)
+
+	def print(self):
+		
+
+		for block in self.blocks:
+			block.draw(graph)
+
 	
 class PARBLOCK:
 	def __init__(self,name):
@@ -64,14 +79,22 @@ class PARBLOCK:
 	def rel(self):
 		availability = 1.0
 		for block in self.blocks:
-            availability *= (1 - block.rel())
+			availability *= (1 - block.rel())
 		return 1.0-availability
 	
 	def draw(self, graph):
-        with graph.subgraph(name='cluster_' + self.name) as c:
-            c.node(self.name, shape='box', label=self.name)
-            for block in self.blocks:
-                block.draw(c)
+		with graph.subgraph(name='cluster_' + self.name) as c:
+			c.node(self.name, shape='box', label=self.name)
+			for block in self.blocks:
+				block.draw(c)
+
+	def print(self):
+		
+
+		for block in self.blocks:
+			block.draw(graph)
+
+		
 
 
 
@@ -88,31 +111,36 @@ seq_block = SEQBLOCK("seq")
 par_block = PARBLOCK("par")
 
 # Verknüpfen der Blöcke zu SEQBLOCK
-seq_block.append(block_R1)
-seq_block.append(block_E)
-seq_block.append(block_R2)
+par_block.append(block_R1)
+par_block.append(block_R2)
 
 # Verknüpfen von SEQBLOCK und PARBLOCK zu PARBLOCK
-par_block.append(seq_block)
-par_block.append(block_A)
+seq_block.append(par_block)
+seq_block.append(block_A)
+seq_block.append(block_E)
 
+graph = graphviz.Digraph(format='jpg')
+# Aufrufen der print() Methode, um das Zuverlässigkeitsdiagramm zu erzeugen
+par_block.print()
 
-#Option1:
+graph.render(filename='reliability_diagram', directory='.', cleanup=True, format='jpg', view=True)
 
-# Ausgabe des Zuverlässigkeitsdiagramms
-par_block.printname()  # Gibt "par" aus
-for block in par_block.blocks:
-    if isinstance(block, SEQBLOCK):
-        block.printname()  # Gibt "seq" aus
-        for seq_block_block in block.blocks:
-            seq_block_block.printname()  # Gibt "R1", "E" und "R2" aus
-    else:
-        block.printname()  # Gibt "A" aus
+##Option1:
 
-# Option2:
-
-# Berechnung der Systemzuverlässigkeit
-system_reliability = par_block.rel()
-
-# Ausgabe der Systemzuverlässigkeit
-print("Systemzuverlässigkeit:", system_reliability)
+## Ausgabe des Zuverlässigkeitsdiagramms
+#par_block.printname()  # Gibt "par" aus
+#for block in par_block.blocks:
+#    if isinstance(block, SEQBLOCK):
+#        block.printname()  # Gibt "seq" aus
+#        for seq_block_block in block.blocks:
+#            seq_block_block.printname()  # Gibt "R1", "E" und "R2" aus
+#    else:
+#        block.printname()  # Gibt "A" aus
+#
+## Option2:
+#
+## Berechnung der Systemzuverlässigkeit
+#system_reliability = par_block.rel()
+#
+## Ausgabe der Systemzuverlässigkeit
+#print("Systemzuverlässigkeit:", system_reliability)
