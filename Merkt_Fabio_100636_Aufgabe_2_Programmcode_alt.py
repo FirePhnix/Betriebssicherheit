473316aae07ceb6ade1dffe2688cc4d4eca46201
FirePhnix <fabio-merkt@t-online.de>
2023-05-25 09:16:04 +0200

    abnahme fertig

--- Merkt_Fabio_100636_Aufgabe_2_Programmcode_alt.py
+++ Merkt_Fabio_100636_Aufgabe_2_Programmcode_alt.py
@@ -44,37 +44,10 @@ class SEQBLOCK:
         dot.attr(rankdir='LR')
         with dot.subgraph(name='cluster_seq') as sg:
             sg.attr(label=self.name)
-            x = "cluster_par"
-            for block in self.blocks:
-                if isinstance(block, PARBLOCK):
-                    x += "xxx"
-                    with sg.subgraph(name=x) as par_sg:
-                        par_sg.attr(label=block.name)
-                        for sub_block in block.blocks:
-                            sub_block.render(par_sg)
-                            seq_list.append(sub_block.getname())
-                else:
-                    block.render(sg)
-                    seq_list.append(block.getname())
-            for block in seq_list:
-                if block[:-1]=="Eingang":
-                    for block2 in seq_list:
-                        if block2[:-1]=="Rechner":
-                            dot.edge(block, block2)
-                elif block[:-1]=="Rechner":
-                    for block2 in seq_list:
-                        if block2[:-1]=="Ausgang":
-                            dot.edge(block, block2)
             
-
-#    def render(self, dot):
-#        dot.attr(rankdir='LR')
-#        with dot.subgraph(name='cluster_seq') as sg:
-#            sg.attr(label=self.name)
-#            
-#            for block in self.blocks:
-#                block.render(sg)
-#                seq_list.append(block.getname())
+            for block in self.blocks:
+                block.render(sg)
+                seq_list.append(block.getname())
 
 class PARBLOCK:
     def __init__(self, name):
@@ -115,51 +88,27 @@ class ZuverlaessigkeitsDiagramm:
         input_name = "Eingang"
         output_name = "Ausgang"
 
-#        for block in par_list:
-#            dot.edge(input_name, block)
-#            dot.edge(block, output_name)
+        for block in par_list:
+            dot.edge(input_name, block)
+            dot.edge(block, output_name)
 
         dot.format = 'png'
         dot.render('reliability_diagram', view=True)
 
-#E = BLOCK('Eingang1', 0.99)
+E = BLOCK('Eingang', 0.99)
 R1 = BLOCK('Rechner1', 0.99)
-#E1 = BLOCK('Eingang1', 0.99)
 R2 = BLOCK('Rechner2', 0.99)
-R3 = BLOCK('Rechner3', 0.99)
-R4 = BLOCK('Rechner4', 0.99)
-R5 = BLOCK('Rechner5', 0.99)
-R6 = BLOCK('Rechner6', 0.99)
-R7 = BLOCK('Rechner7', 0.99)
-E1 = BLOCK('Eingang1', 0.99)
-E2 = BLOCK('Eingang2', 0.99)
-E3 = BLOCK('Eingang3', 0.99)
-
-A1 = BLOCK('Ausgang1', 0.99)
-A2 = BLOCK('Ausgang2', 0.99)
+A = BLOCK('Ausgang', 0.99)
 
 seq = SEQBLOCK("Alle")
 par = PARBLOCK("Alle_Rechner")
-par_2 = PARBLOCK("2")
-par_3 = PARBLOCK("3")
-
-
 
 par.append(R1)
 par.append(R2)
-par_2.append(E1)
-par_2.append(E2)
-par_2.append(E3)
 
-par_3.append(A1)
-par_3.append(A2)
-
-
-seq.append(par_2)
-#seq.append(E1)
+seq.append(E)
 seq.append(par)
-seq.append(par_3)
-
+seq.append(A)
 
 par_list = []
 seq_list = []
